68d998a8c5ab1d9b57ebf45edc362762f6addc1f
Hary051 <petrharapat05@gmail.com>
2024-09-10 09:24:17 +0200

    Přidejte soubory projektu.

---
+++ nový začátek/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[] { 1,5,9 };
+        double a=Avg(numbers);
+        Console.WriteLine(a);
+       
+    }
+    private static double Avg(int[] numbers)
+    {
+        int result = 0;
+        int avg;
+        for (int i = 0;i < numbers.Length; i++)
+        {
+            result += numbers[i];
+        }
+        double avg1= result / numbers.Length;
+        return avg1;
+    }
+
+}
