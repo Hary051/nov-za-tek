f103b08656e9aece0b563d2b7c5ae1063d26c62d
Hary051 <petrharapat05@gmail.com>
2024-09-17 09:24:07 +0200

    fixes #2

--- nový začátek/Program.cs
+++ nový začátek/Program.cs
@@ -2,12 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        int[] numbers = new int[] { 1,5,9 };
-        double a=Avg(numbers);
-        Console.WriteLine(a);
-       
-    }
-    private static double Avg(int[] numbers)
+        Console.WriteLine("12211");
+     static double Avg(int[] numbers)
     {
         int result = 0;
         int avg;
@@ -18,6 +14,7 @@
         double avg1= result / numbers.Length;
         return avg1;
     }
+
     void Plus()
     {
         Console.WriteLine(1);
@@ -26,5 +23,6 @@
     {
         Console.Write(1);
     }
+    }
 }
-new important update 1
+111
